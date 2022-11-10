c8f86b3ce7953a2cca583f2fec1f11ecb671b341
nicolascaporaso <nicolascaporaso@gmail.com>
2022-11-09 23:29:52 -0300

    segunda preentrega

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,7 @@ import '../assets/css/Footer.css'
 export default function Footer() {
     return (
         <footer className="footer">
-            <div class="footer__div">
+            <div className="footer__div">
                 <h4 className="footer__div__titulo">Para estar al dia de nuestras novedades, visita nuestras redes sociales.</h4>
             </div>
         </footer>
--- src/components/ItemList.jsx
+++ src/components/ItemList.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Link } from "react-router-dom";
 import '../assets/css/itemListContainer.css'
+import Item from './item';
 
 
 export default function ItemList({productos}) {
@@ -12,12 +13,7 @@ export default function ItemList({productos}) {
                 <div className="tienda__grid">
                     {!productos.length && "Loading..."}
                     {productos.map((item) => (
-                        <div className='tienda__grid__card' key={item.id}>
-                            <h3 className='tienda__grid__card__titulo'>{item.nombre} </h3>
-                            <p className='tienda__grid__card__parrafo'>Precio: ${item.precio}</p>
-                            <img className='tienda__grid__card__img' src={item.img} alt="Planta acuatica " />
-                            <div className='link'><Link  to={"/item/" + item.id}>ir al item</Link></div>
-                        </div>
+                        <Item key={item.id} item={item}/>
                     ))}
                 </div>
             </div>
