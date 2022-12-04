b85064865f0ec72add0d564232463eefe12606eb
nicolascaporaso <nicolascaporaso@gmail.com>
2022-12-04 13:31:08 -0300

    correccion spinner

--- src/components/ItemDetail.jsx
+++ src/components/ItemDetail.jsx
@@ -2,6 +2,7 @@ import React, { useContext, useState } from 'react';
 import { Link } from "react-router-dom";
 import { contextGeneral } from "../components/cartContext.jsx";
 import Itemcount from './ItemCount';
+import CircularIndeterminate from './spinner';
 
 export default function ProductoDetail({product}) {
   const [goCart, setGoCart]= useState(false);
@@ -17,8 +18,8 @@ export default function ProductoDetail({product}) {
     <div className='tienda'>
       <h2 className='tienda__titulo'>Descripcion detallada del producto.</h2>
       <div>
+      {!product.id && <CircularIndeterminate />}
         <div className="tienda__grid">
-          {!product.id && "Loading..."}
           <div className='tienda__grid__card' key={product.id}>
             <h3 className='tienda__grid__card__titulo'>{product.name} </h3>
             <img alt="imagen de planta acuatica" className='tienda__grid__card__img' src={product.image} />
@@ -26,7 +27,7 @@ export default function ProductoDetail({product}) {
             <h4 className='tienda__grid__card__parrafo'>Descripcion: {product.description}</h4>
             <p className='tienda__grid__card__parrafo'>Stock: {product.amount}</p>
             
-            {goCart? <Link to='/carrito'>Terminar Compra</Link> : <div><Itemcount max={product.amount} min={1} onAdd={onAdd}/></div>}
+            {goCart? <Link className='link' to='/carrito'><h4 style={{border:'2px solid black', textDecoration: 'none' }}>Terminar Compra</h4></Link> : <div><Itemcount max={product.amount} min={1} onAdd={onAdd}/></div>}
           </div>
         </div>
       </div> 
--- src/components/ItemList.jsx
+++ src/components/ItemList.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import '../assets/css/itemListContainer.css';
 import Item from './item';
+import CircularIndeterminate from './spinner';
 
 
 export default function ItemList({products}) {
@@ -11,7 +12,7 @@ export default function ItemList({products}) {
             <h2 className='tienda__titulo'>Bienvenido a nuestra tienda, Podra adquirir variedad en plantas de acuario, de la mejor calidad, y con el envio mas seguro.</h2>
             <div>
                 <div className="tienda__grid">
-                    {! products.length && "Loading..."}
+                    {! products.length && <CircularIndeterminate />}
                     { products.map((item) => (
                         <Item key={item.id} item={item}/>
                     ))}
